5f9d020740ff9a4701edd4ce6ebf754a0ab6d9c3
darkextremepro <darkextreme@live.com>
2022-03-11 16:34:45 -0300

    componente final

---
+++ Componente CmdRead [F]/CmdRead/IHandle.pas
@@ -0,0 +1,14 @@
+unit IHandle;
+
+interface
+  type
+    IHandleCmd = Interface
+      ['{CB248F92-D75D-4550-BFC4-4001D9CD9E68}']
+       function TempFileName( FileName : String) : IHandleCmd; overload;
+       function TempFileName : String;overload;
+       function PathScript(PathFile : String) :IHandleCmd; overload;
+       function PathScript : String;overload;
+    End;
+implementation
+
+end.
